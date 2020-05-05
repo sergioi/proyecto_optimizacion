401d2729d8889c7e73657a6aa40a10edd4600d4e
sergioi <sergio.coatlperez@itsva.edu.mx>
2020-05-04 22:45:36 -0500

    administrador

---
+++ ProyectoOptimizacion/page/admin/crud1.php
@@ -0,0 +1,55 @@
+<?php
+
+include_once '../../db.php';
+
+/* Codigo para Insertar Datos */
+if(isset($_POST['guardar']))
+{
+		
+	 echo "Guardandoooooooooooooooooooo";
+     $descipcion = $MySQLiconn->real_escape_string($_POST['desProd']);
+     $descipcion = $MySQLiconn->real_escape_string($_POST['producto']);
+     $precioCompra = $MySQLiconn->real_escape_string($_POST['preComp']);
+     $precioVenta = $MySQLiconn->real_escape_string($_POST['preVen']);
+     $stockActual = $MySQLiconn->real_escape_string($_POST['skoActual']);
+     $stockMinimo = $MySQLiconn->real_escape_string($_POST['skoMini']);
+     $stockMaximo = $MySQLiconn->real_escape_string($_POST['skoMax']);
+     $estado = $MySQLiconn->real_escape_string($_POST['estProd']);
+     $fechaVencimiento = $MySQLiconn->real_escape_string($_POST['fechVenc']);
+     $ipcategoriass = $MySQLiconn->real_escape_string($_POST['catperte']);
+
+
+  $SQL = $MySQLiconn->query("INSERT INTO producto (producto, descripcion, precioCompra, precioVenta, stockActual, stockMinimo, stockMaximo, estado, fechaVencimiento, idcategoria) VALUES('$producto',$descipcion','$precioCompra','$precioVenta','$stockActual','$stockMinimo','$stockMaximo','$estado','$fechaVencimiento','$ipcategoriass')");
+  
+  if(!$SQL)
+  {
+   echo $MySQLiconn->error;
+  } 
+  header("Location:index.php");
+}
+
+/* Codigo para eliminar Datos */
+if(isset($_GET['eliminar']))
+{
+ $SQL = $MySQLiconn->query("DELETE FROM producto WHERE idproducto=".$_GET['eliminar']);
+ header("Location:index.php");
+}
+
+
+/* Codigo para Editar Datos */
+if(isset($_GET['editar']))
+{
+
+ $SQL = $MySQLiconn->query("SELECT * FROM producto WHERE idproducto=".$_GET['editar']);
+ $getROW = $SQL->fetch_array();
+}
+
+/* Codigo para Actualizar Datos */
+if(isset($_POST['actualizar']))
+{
+ $SQL = $MySQLiconn->query("UPDATE producto SET producto='".$_POST['producto']."',descripcion='".$_POST['desProd']."', precioCompra='".$_POST['preComp']."', precioVenta='".$_POST['preVen']."', stockActual='".$_POST['skoActual']."', stockMinimo='".$_POST['skoMini']."', stockMaximo='".$_POST['skoMax']."', estado='".$_POST['estProd']."',  fechaVencimiento='".$_POST['fechVenc']."' WHERE idproducto=".$_GET['editar']);
+ header("Location:index.php");
+}
+
+
+?>
---
+++ ProyectoOptimizacion/page/admin/index.php
@@ -0,0 +1,204 @@
+<?php
+include_once '../../db.php';
+include_once 'crud1.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link  rel = " stylesheet " href = " https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css " >
+    < script  src = " https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js " > </ script >
+    < script  src = " https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js " > </ script >
+    < script  src = " https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js " > </ script >
+
+    <title>Administrador</title>
+</head>
+
+<body>
+    <div>
+        <a href="index.php">NUEVO PRODUCTO</a>
+        <a href="user.php">NUEVO USER</a>
+    </div>
+
+    <!--Nombre Empresa-->
+    <br>
+    <div class="container text-center">
+        <h1>Hamburguesa "El paso"</h1>
+    </div>
+    <br>
+
+    <div class="container-fluid">
+        <div>
+            <h2 class="text-center">Nuevo producto</h2>
+        </div>
+
+
+        <form method="post">
+            <div class="form-group">
+                <label class="control-label">Producto:</label>
+                <div class="control-label">
+                    <input type="text" class="form-control text-uppercase" name="producto"
+                        placeholder="Nombre de producto:"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['producto'];  ?>" />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="control-label">Descripcion:</label>
+                <div class="control-label">
+                    <input type="text" class="form-control text-uppercase" name="desProd" placeholder="Descripcion"
+                        value="<?php
+                                                                                                              if (isset($_GET['editar']))
+                                                                                                                echo $getROW['descripcion'];
+                                                                                                              ?>" />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="control-label">Precio de compra:</label>
+                <div class="col">
+                    <input type="number" step="0.001" class="form-control text-uppercase" name="preComp"
+                        placeholder="Precio de compra(decimal):"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['precioCompra'];  ?>" />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="control-label">Precio de venta:</label>
+                <div class="col">
+                    <input type="number" step="0.01" class="form-control text-uppercase" name="preVen"
+                        placeholder="Precio de venta(decimal):"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['precioVenta'];  ?>" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label">Stock stockActual:</label>
+                <div class="col">
+                    <input type="number" class="form-control text-uppercase" name="skoActual"
+                        placeholder="Stock stockActual(entero):"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['stockActual'];  ?>" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label">Stock Minimo:</label>
+                <div class="col">
+                    <input type="number" class="form-control text-uppercase" name="skoMini"
+                        placeholder="Stock Minimo(entero):"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['stockMinimo'];  ?>" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label">Stock Maximo:</label>
+                <div class="col">
+                    <input type="number" class="form-control text-uppercase" name="skoMax"
+                        placeholder="Stock Maximo(entero):"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['stockMaximo'];  ?>" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label">Estado:</label>
+                <div class="col">
+                    <input type="text" class="form-control text-uppercase" name="estProd" placeholder="Estado(M O B):"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['estado'];  ?>" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label">Fecha de vencimiento:</label>
+                <div class="col">
+                    <input type="date" class="form-control text-uppercase" name="fechVenc" placeholder="2019-02-01:"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['fechaVencimiento'];  ?>" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label">Categoria perteneciente:</label>
+                <div class="col">
+                    <input type="number" class="form-control text-uppercase" name="catperte"
+                        placeholder="categoria a la que pertenece:"
+                        value="<?php if (isset($_GET['editar'])) echo $getROW['idcategoria'];  ?>" />
+                </div>
+            </div>
+            <div class="form-group">
+                <?php
+        if (isset($_GET['editar'])) {
+        ?>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-success" name="actualizar">Actualizar</button>
+                </div>
+                <?php
+        } else {
+        ?>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-success" name="guardar">Guardar</button>
+                </div>
+                <?php
+        }
+        ?>
+            </div>
+        </form>
+    </div>
+
+
+    <!--Table-->
+    <div class="container-fluid">
+        <table class="table table-hover table-bordered shadow p-3 mb-5 bg-white rounded">
+            <tr>
+                <th>Id</th>
+                <th>Descripcion</th>
+                <th>Producto</th>
+                <th>Precio de compra</th>
+                <th>Precio de venta</th>
+                <th>Stock actual</th>
+                <th>Stock Minimo</th>
+                <th>Stock Maximo</th>
+                <th>Estado</th>
+                <th>Fecha de vencimiento</th>
+                <th>Categoria perteneciente</th>
+                <th>Acciones</th>
+            </tr>
+
+            <?php
+
+   $res = $MySQLiconn->query("SELECT * FROM producto");
+   while($row=$res->fetch_array())
+   {
+    ?>
+            <tr>
+                <td> <?php echo $row['idproducto']; ?> </td>
+                <td> <?php echo $row['descripcion']; ?> </td>
+                <td> <?php echo $row['producto']; ?></td>
+                <td> $ <?php echo $row['precioCompra']; ?> </td>
+                <td> $ <?php echo $row['precioVenta']; ?> </td>
+                <td> <?php echo $row['stockActual']; ?> </td>
+                <td> <?php echo $row['stockMinimo']; ?> </td>
+                <td> <?php echo $row['stockMaximo']; ?> </td>
+                <td> <?php echo $row['estado']; ?> </td>
+                <td> <?php echo $row['fechaVencimiento']; ?> </td>
+                <td> <?php echo $row['idcategoria']; ?> </td>
+                <td>
+                    <a href="?editar=<?php
+                              echo $row['idproducto'];
+                              ?>" onclick="return confirm('¿Deseas Editarlo ?'); ">
+
+                        <span>Editar</span>
+
+                    </a>
+
+                    <a href="?eliminar=<?php echo $row['idproducto']; ?>"
+                        onclick="return confirm('¿Seguro deseas eliminarlo?'); ">
+                        <span>trash</span>
+                    </a>
+                </td>
+            </tr>
+            <?php
+   }
+   ?>
+        </table>
+    </div>
+
+
+</body>
+
+</html>
